f83c8830c1d769893cfe0690f2dda9d148c5480d
Mark K <mark.keinhoerster@gmail.com>
2014-07-28 22:53:14 +0200

    watch list improvements

--- WatchTasks.py
+++ WatchTasks.py
@@ -7,60 +7,73 @@ import datetime
 def watchTasks(event):
     root = Tk()
     root.title("Watch Task")
-    screenResY = root.winfo_screenheight()
-    root.geometry("200x100")# + str(screenResY))
     root.resizable(0, 0)
-
-    vscrollbar = Scrollbar(root)
-    vscrollbar.pack(side=RIGHT, fill=Y)
-
-    canvas = Canvas(root, yscrollcommand=vscrollbar.set)
-    canvas.pack(side=LEFT, fill=Y)
-
-    vscrollbar.config(command=canvas.yview)
-
-    # make the canvas expandable
-    root.grid_rowconfigure(0, weight=1)
-    root.grid_columnconfigure(0, weight=1)
-
-    #
-    # create canvas contents
-    frame = Frame(canvas)
-    frame.rowconfigure(1, weight=2)
-    frame.columnconfigure(1, weight=1)
-
     connection = sqlite3.connect("stasks.db")
     cursor = connection.cursor()
     rows = cursor.execute("select * from tasks order by done ASC, insertTS DESC")
     connection.commit()
 
+    entries = set()
     for row in rows:
-
         done = row[3]
-        color = "black"
-        if(done == 1):
-            color = "grey"
-
-        container = LabelFrame(frame, bd=2)
-
         date = datetime.datetime.strptime(row[2], "%Y-%m-%d %H:%M:%S")
         inserted = date
-        insertedLabel = Label(container, text=inserted, fg=color, justify=LEFT, anchor=W)
-        insertedLabel.pack(fill=X)
-
         title = row[1]
-        titleLabel = Label(container, text=title, fg=color, justify=LEFT, anchor=W)
-        titleLabel.pack(fill=X)
-
-        container.pack(fill=X)
-
-    canvas.create_window(0, 0, anchor=NW, window=frame)
-
-    frame.update_idletasks()
-
-    canvas.config(scrollregion=canvas.bbox("all"))
+        entries.add((title, inserted, done))
 
     def dispose(event):
         root.destroy()
 
+    global begin
+    begin = 0
+    global end
+    end = 5
+
+    def moveDown(event):
+        global end
+        global begin
+        if end + 1 < len(entries):
+            begin += 1
+            end += 1
+        show(begin, end)
+
+    def moveUp(event):
+        global end
+        global begin
+        if begin - 1 >= 0:
+            begin -= 1
+            end -= 1
+        show(begin, end)
+
+    all = set()
+    def show(begin, end):
+        for container in all:
+            container.grid_remove()
+        all.clear()
+        i = 0
+        for entry in entries:
+            if (i >= begin) and (i <= end):
+                done = entry[2]
+                color = "black"
+                if done == 1:
+                    color = "grey"
+                container = LabelFrame(root, bd=0, bg="grey", height=2)
+                inserted = entry[1]
+                insertedLabel = Label(container, text=inserted, fg=color, justify=LEFT, anchor=W, width=55)
+                insertedLabel.pack(fill=X)
+                title = entry[0]
+                if len(title) > 50:
+                    title = title[:51]
+                    title += "..."
+                titleLabel = Label(container, text=title, fg=color, justify=LEFT, anchor=W, width=55)
+                titleLabel.pack(fill=X)
+                container.grid(row=i, column=1, sticky=W)
+                all.add(container)
+            i += 1
+
+
     root.bind('<Return>', dispose)
+    root.bind('<w>', moveUp)
+    root.bind('<s>', moveDown)
+
+    show(begin, end)
